deea3dd74ee4a6326a9b998d63e514e26cb5f929
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2020-06-26 19:51:57 +0200

    Added inverse pendulum.

--- thesis/figures/introduction/pendulum-motion.py
+++ thesis/figures/introduction/pendulum-motion.py
@@ -4,62 +4,66 @@ import scipy.integrate as sci
 
 
 
-ode = lambda t, y: np.array([y[1], -np.sin(y[0])])
+ode = lambda t, y: np.array([y[1], np.sin(y[0])])
 
-small_angle_solution = lambda x0, xd0, t: x0 * np.cos(t) + xd0 * np.sin(t)
+small_angle_solution = lambda x0, xd0, t: 0.5 * np.exp(-t) * (x0 + np.exp(2 * t) * x0 - xd0 + np.exp(2 * t) * xd0)
 
 
 
 t_start = 0
-t_end = 100
+t_end = 10
 t_range = np.arange(t_start, t_end, 0.01)
 
-yticks = [-np.pi / 2.0, 0, np.pi / 2.0]
-ytickslabels = [r'$-\frac{\pi}{4}$', r'$0$', r'$\frac{\pi}{4}$']
+yticks = [0, np.pi / 2.0, np.pi, 3 * np.pi / 2.0, 2 * np.pi]
+ytickslabels = [r'$0$', r'$\frac{\pi}{2}$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$']
 
 
 
 def solve_ivp(initial_displacement, initial_velocity):
-	sol_numerical = sci.solve_ivp(ode, y0 = (initial_displacement, initial_velocity), t_span = (t_start, t_end), t_eval = t_range).y[0, :]
+	sol_numerical = sci.solve_ivp(ode, y0 = (initial_displacement, initial_velocity), t_span = (t_start, t_end), t_eval = t_range, method = 'Radau').y[0, :]
 	sol_approx = small_angle_solution(initial_displacement, initial_velocity, t_range)
-	error = sol_numerical - sol_approx
+	error = (sol_approx - sol_numerical) ** 2
 	return sol_numerical, sol_approx, error
 
 
 
-def plot_solution(initial_displacement, initial_velocity, initial_displacement_latex = None, initial_velocity_latex = None):
-	sol_numerical, sol_approx, error = solve_ivp(initial_displacement, initial_velocity)
+initial_displacement = 0.0872665
+initial_velocity = 0
+tolerance = 1e-3
+sol_numerical, sol_approx, error = solve_ivp(initial_displacement, initial_velocity)
 
-	fig, (ax1, ax2) = plt.subplots(2, 1, sharex = True, figsize = (10, 8))
-	ax1.plot(t_range, sol_numerical, label = 'Numerical Solution (RK45)')
-	ax1.plot(t_range, sol_approx, label = 'Small Angle Solution (Analytical)')
-	ax2.plot(t_range, error, color = 'red', label = 'Error')
-	ax1.legend()
-	ax2.legend()
-	#ax1.set_yticks(yticks)
-	#ax2.set_yticks(yticks)
-	#ax1.set_yticklabels(ytickslabels)
-	#ax2.set_yticklabels(ytickslabels)
-	ax1.set_ylabel(r'$\varphi$')
-	ax2.set_ylabel(r'$\varphi$')
-	ax2.set_xlabel(r'$t$')
-	initial_displacement_latex = str(initial_displacement) if initial_displacement_latex is None else initial_displacement_latex
-	initial_velocity_latex = str(initial_velocity) if initial_velocity_latex is None else initial_velocity_latex
-	fig.suptitle(r'Comparison of Solutions for $\varphi(0) = ' + initial_displacement_latex + r'$ and $\dot{\varphi}(0) = ' + initial_velocity_latex + '$')
-	fig.savefig('generated/pendulum-motion-%s-%s.pdf' % (str(int(initial_displacement * 100)), str(int(initial_velocity * 100))))
-	fig.show()
+tolerance_violation_t = None
+tolerance_violation_err = None
+for t, err in zip(t_range, error):
+	if err > tolerance:
+		tolerance_violation_t = t
+		tolerance_violation_err = err
+		break
+if tolerance_violation_t is None or tolerance_violation_err is None:
+	raise Exception('Something has to be wrong! The small angle approx. is not so good.')
 
 
+fig, ax = plt.subplots()
+ax.vlines(tolerance_violation_t, min(yticks), max(yticks), ls = 'dashed', color = 'grey', alpha = 0.25, label = 'Distance greater than ' + str(tolerance))
+ax.plot(t_range, sol_numerical, label = 'Numerical Solution (Radau)')
+ax.plot(t_range, sol_approx, label = 'Small Angle Solution (Analytical)')
+ax.legend()
+ax.set_xlim([t_start, t_end])
+ax.set_ylim([min(yticks), max(yticks)])
+ax.set_yticks(yticks)
+ax.set_yticklabels(ytickslabels)
+ax.set_xlabel(r'$t$')
+ax.set_ylabel(r'$\varphi$')
+ax.set_title(r'Numerical and Approximate Solution for $\varphi(0) = 5\degree$, $\dot{\varphi} = 0$')
+fig.savefig('generated/pendulum-motion-solutions.pdf')
 
-plot_solution(0.0, 0.1)
-plt.show()
-
-
-
-#initial_velocity = 0.0
-#for initial_displacement in [0.01, 0.05, 0.1]:
-#	plot_solution(initial_displacement, 0.0)
-#
-#initial_displacement = 0.0
-#for initial_velocity in [0.01, 0.05, 0.1, 0.5]:
-#	plot_solution(0.0, initial_velocity)
+fig, ax = plt.subplots()
+ax.vlines(tolerance_violation_t, min(error), max(error), ls = 'dashed', color = 'grey', alpha = 0.25, label = 'Distance greater than ' + str(tolerance))
+ax.plot(t_range, error, color = 'red', label = 'Squared Difference')
+ax.legend()
+ax.set_xlim([t_start, t_end])
+ax.set_yscale('log')
+ax.set_xlabel(r'$t$')
+ax.set_ylabel(r'$(\hat{\varphi} - \varphi)^2$')
+ax.set_title(r'Squared Difference of Solutions for $\varphi(0) = 5\degree$, $\dot{\varphi} = 0$')
+fig.savefig('generated/pendulum-motion-difference.pdf')
