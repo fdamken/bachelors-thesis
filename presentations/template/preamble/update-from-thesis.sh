adb59dd8f81fe8909af375a5e67431378d755898
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2020-12-07 14:56:20 +0100

    Removed unnecessary presentations template.

--- presentations/template/preamble/update-from-thesis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp ../../../thesis/preamble/packages.tex .
-# Remove the theorem definitions as beamer already defines them.
-sed -r 's@^\\newtheorem\{(theorem|definition|lemma)\}.+$@@g' ../../../thesis/preamble/styles.tex >styles.tex
